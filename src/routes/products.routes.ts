65b9f46aa10b8260941699cecf0a52cc0cfe8c34
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2025-06-19 10:42:52 -0300

    feat:  created the functionality that checks Authorization and Authentication

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import { ProductsController } from "@/controllers/products-controller";
 import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated";
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
 
 const productsRoutes = Router();
-const productsController = new ProductsController();
 
+const productsController = new ProductsController();
+// Dessa forma abaixo estaríamos aplicando autorização em todas as rotas
+// productsRoutes.use(verifyUserAuthorization(["sale", "admin"]));
 productsRoutes.get("/", productsController.index);
-productsRoutes.post("/", ensureAuthenticated, productsController.create);
+productsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  verifyUserAuthorization(["sale", "admin"]),
+  productsController.create
+);
 
 export { productsRoutes };
